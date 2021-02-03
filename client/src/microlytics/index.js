6df8a3ea4f31d60af220f96ef5fb90d8e09a2de0
Maharshi Patel <maharshi365@gmail.com>
2021-02-02 21:22:01 -0500

    added user initialize

--- client/src/microlytics/index.js
+++ client/src/microlytics/index.js
@@ -8,43 +8,54 @@ import Microlytics from './components/Microlytics';
 var trackingKey = '';
 var sessionId = uuid();
 var baseUrl = 'https://www.edisonbox.ca/api/v1/logs/arr';
+var userObj = {};
 export var MICROLYTICS_EVENTS = [];
 
 const initialize = (key, url = null) => {
   trackingKey = key;
   baseUrl = url ? url : baseUrl;
   MICROLYTICS_EVENTS = [];
-
-  sendData();
 };
 
 const getValues = () => {
   return [trackingKey, sessionId, baseUrl, MICROLYTICS_EVENTS];
 };
 
+const initializeUser = (user) => {
+  userObj = user;
+};
+
 // export functions
-function sendHoverEvents(session, key) {
+function sendHoverEvents(session, key, user) {
   if (MICROLYTICS_EVENTS.length > 0 && trackingKey !== '') {
     const eventsLength = MICROLYTICS_EVENTS.length;
 
     let eventArr = MICROLYTICS_EVENTS.slice(0, eventsLength - 1);
     MICROLYTICS_EVENTS = MICROLYTICS_EVENTS.slice(eventsLength);
 
+    const body = {
+      dataArray: eventArr,
+      sessionId: session,
+      trackingKey: key
+    };
+
+    if (user !== {}) {
+      body.user = user;
+    }
+
     fetch(baseUrl, {
       method: 'POST',
-      body: JSON.stringify({
-        dataArray: eventArr,
-        sessionId: session,
-        trackingKey: key,
-      }),
-      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+      body: JSON.stringify(body),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' }
     }).catch((err) => console.log(err));
   }
 }
 
 function sendData() {
-  sendHoverEvents(trackingKey, sessionId);
+  sendHoverEvents(trackingKey, sessionId, userObj);
   setTimeout(sendData, 5000);
 }
 
-export { initialize, getValues, Microlytics };
+sendData();
+
+export { initialize, getValues, Microlytics, initializeUser };
